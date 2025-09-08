e601dbae465b9bc8e01a82316cff0c77d8ab5c7a
Timur <muutim@gmail.com>
2025-09-08 14:13:19 +0200

    vite and npm updates

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -1,14 +1,20 @@
 from .base import *
 DEBUG = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://0.0.0.0:3000",  
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",   # Vite dev
+    "http://127.0.0.1:3000",   # Vite dev (alt)
+    "http://0.0.0.0:3000",   # Vite dev (alt)
+    "http://localhost:3001",   # Nginx demo
+    "http://127.0.0.1:3001",   # Nginx demo (alt)
+    "http://0.0.0.0:3001"  # Nginx demo (alt)
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://0.0.0.0:3000",
+    "http://localhost:3000",   # Vite dev
+    "http://127.0.0.1:3000",   # Vite dev (alt)
+    "http://0.0.0.0:3000",   # Vite dev (alt)
+    "http://localhost:3001",   # Nginx demo
+    "http://127.0.0.1:3001",   # Nginx demo (alt)
+    "http://0.0.0.0:3001"  # Nginx demo (alt)
 ]
--- backend/core/settings/prod.py
+++ backend/core/settings/prod.py
@@ -2,6 +2,18 @@ from .base import *
 DEBUG = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://0.0.0.0:3000",  
+    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    "https://mustafin.info",
+    "https://www.mustafin.info",
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    "https://mustafin.info",
+    "https://www.mustafin.info",
 ]
